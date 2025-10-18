ef50213a1d2643b6e26bdd787f0264909e2b1462
KhalidD98 <60935358+KhalidD98@users.noreply.github.com>
2025-10-18 14:08:20 -0500

    - added tailwind to project - created two sided files to populate with info

---
+++ src/LeftSide.jsx
@@ -0,0 +1,10 @@
+function LeftSide() {
+  return (
+    <div className="w-1/3 h-full bg-gray-100">
+      Left
+    </div>
+  )
+}
+
+export default LeftSide
+
---
+++ src/RightSide.jsx
@@ -0,0 +1,10 @@
+function RightSide() {
+  return (
+    <div className="w-2/3 h-full bg-white">
+      Right
+    </div>
+  )
+}
+
+export default RightSide
+
